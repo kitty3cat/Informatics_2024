a4a8dd4ea0de4ed23d28f084ccc7f7ee96394796
kitty3cat <yupiyo1116@gmail.com>
2024-12-21 09:24:33 +0300

    исправила ошибки

--- golang/main.go
+++ golang/main.go
@@ -7,6 +7,5 @@ import(
 
 func main() {
 	fmt.Println("Valova Alyona Andreevna")
-	fmt.Println("Task А:\n", lab4.TaskA(0.95, 2.0, 1.25, 2.75, 0.3))
-	fmt.Println("Task В:\n", lab4.TaskB(2.0, 0.95, [5]float64{2.2, 3.78, 4.51, 6.58, 1.2}))
+	lab4.RunLab4()
 }
