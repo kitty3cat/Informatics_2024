3041467691cc7294797c097494df43aa8ccd3fd6
kitty3cat <yupiyo1116@gmail.com>
2024-12-27 00:27:16 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -2,10 +2,12 @@ package main
 
 import(
 	"fmt"
+	"isuct.ru/informatics2022/lab6"
 	"isuct.ru/informatics2022/lab4"
 )
 
 func main() {
 	fmt.Println("Valova Alyona Andreevna")
 	lab4.RunLab4()
+	lab6.Start6lab()
 }
